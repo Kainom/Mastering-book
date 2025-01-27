a46a1f4830f756ac0622062c9ed50dbf9ca604a9
Kainom <eleuteriokaina@gmail.com>
2025-01-27 15:40:49 -0300

    Auth and Keycloack

--- securtiy/src/main/java/kainom/com/security/config/security/SecurityConfig.java
+++ securtiy/src/main/java/kainom/com/security/config/security/SecurityConfig.java
@@ -18,7 +18,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults());
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
+
 }
